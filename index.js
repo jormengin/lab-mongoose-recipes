f0a1c72dfad3531cff327dd683c3e5f2a4ef74d1
Jorge Mendez <jorge.mendezgines@gmail.com>
2023-01-13 16:02:42 +0100

    done

--- index.js
+++ index.js
@@ -1,23 +1,63 @@
 const mongoose = require('mongoose');
-
-// Import of the model Recipe from './models/Recipe.model.js'
+mongoose.set("strictQuery", false);
 const Recipe = require('./models/Recipe.model');
-// Import of the data from './data.json'
 const data = require('./data');
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = "mongodb://127.0.0.1:27017/recipe-app";
 
-// Connection to the database "recipe-app"
+
 mongoose
   .connect(MONGODB_URI)
-  .then(x => {
+  .then((x) => {
     console.log(`Connected to the database: "${x.connection.name}"`);
-    // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
+    return Recipe.deleteMany();
+  })
+  //ITERATION 2
+  .then(() => {
+    return Recipe.create({
+      title: "Arepa Reina Pepiada",
+      level: "Amateur Chef",
+      ingredients: [
+        "2 Cups of Water",
+        "1 cup P.A.N White Corn Meal",
+        "2 HASH Avocados",
+        "2 Tbls Mayonese",
+        "1 Tbs of Salt",
+        "2 Cooked and Shreded Chicken Breast",
+        "2 tbs Olive Oil",
+      ],
+      cuisine: "Venezuelan",
+      dishType: "breakfast",
+      image:
+        "https://media-cldnry.s-nbcnews.com/image/upload/newscms/2015_18/515696/arepa_reina_pepiada-today-tease-vr-150429.jpg",
+      duration: 20,
+      creator: "My Grandma",
+    });
+  })
+  .then((recipe) => {
+    console.log(recipe.title);
+  })
+  //ITERATION 3
+  .then(() => {
+    return Recipe.insertMany(data);
+  })
+  //ITERATION 4
+  .then(() => {
+    return Recipe.findOneAndUpdate(
+      { title: "Rigatoni alla Genovese" },
+      { $set: { duration: 100 } }
+    );
+  })
+  //ITERATION 5
+  .then(() => {
+    return Recipe.deleteOne({ title: "Carrot Cake" });
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    //ITERATION 6
+    mongoose.connection.close(() => {
+      console.log("CONNECTION WAS CLOSED SUCCESSFULLY");
+    });
   })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
+  .catch((error) => {
+    console.error("Error connecting to the database", error);
   });
--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -1,8 +1,53 @@
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+const Schema = mongoose.Schema; // lo mismo que-> const { Schema } = mongoose.
 
 const recipeSchema = new Schema({
-  // TODO: write the schema
+  title: {
+    type: String,
+    required: [true,'Title is mandatory'],
+    unique: true,
+  },
+  level: {
+    type: String,
+    enum: {
+      values: ["Easy Peasy", "Amateur Chef", "UltraPro Chef"],
+      message: 'Pick a valid level for the recipe'
+    }
+  },
+  ingredients: {
+    type: [String],
+  },
+  cuisine: {
+    type: String,
+    required: [true, 'Please indicate the type of cuisine'],
+  },
+  dishType: {
+    type: String,
+    enum: [
+      "breakfast",
+      "main_course",
+      "soup",
+      "snack",
+      "drink",
+      "dessert",
+      "other",
+    ],
+  },
+  image: {
+    type: String,
+    default: "https://images.media-allrecipes.com/images/75131.jpg",
+  },
+  duration: {
+    type: Number,
+    min:0
+  },
+  creator: {
+    type: String
+  },
+  created: {
+    type: Date,
+    default: Date.now()
+  }
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);
